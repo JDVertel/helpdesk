2edebab91326d85fcaf0453faaf702bfa1781689
juan david vertel holguin <127699117+JDVertel@users.noreply.github.com>
2025-01-21 20:32:25 +0000

    update 1

---
+++ src/components/admin/index.vue
@@ -0,0 +1,15 @@
+<template>
+  <h1>admin</h1>
+</navbar>
+</template>
+
+<script>
+import navbar from '../navbar.vue';
+import navbarVue from '../navbar.vue';
+export default {
+  components: { navbar },};
+import navbarVue from navbarVue
+</script>
+
+<style>
+</style>
---
+++ src/components/super/index.vue
@@ -0,0 +1,10 @@
+<template>
+  <h1>superusuario helpdesk</h1>
+</template>
+
+<script>
+export default {};
+</script>
+
+<style>
+</style>
